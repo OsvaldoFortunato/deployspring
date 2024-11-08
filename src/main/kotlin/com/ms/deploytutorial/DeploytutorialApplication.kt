f3ad5863bb45462226de3bce53ab083884e04a19
Osvaldo Fortunato <fortunatocasanova@outlook.com>
2024-11-08 10:28:16 +0100

    novo

--- src/main/kotlin/com/ms/deploytutorial/DeploytutorialApplication.kt
+++ src/main/kotlin/com/ms/deploytutorial/DeploytutorialApplication.kt
@@ -2,10 +2,20 @@ package com.ms.deploytutorial
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DeploytutorialApplication
 
 fun main(args: Array<String>) {
 	runApplication<DeploytutorialApplication>(*args)
+}
+
+@RestController
+class HelloController{
+	@GetMapping("/hello")
+	fun hello():String{
+		return "hello osvaldo"
+	}
 }
